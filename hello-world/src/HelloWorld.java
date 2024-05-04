70d1c1ac00e88df9cfa7d7f52058a0328998fdb9
Julian Swerdlin <julianswer36@gmail.com>
2024-05-03 23:19:40 -0300

    intial commit

---
+++ hello-world/src/HelloWorld.java
@@ -0,0 +1,27 @@
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        int newVariable = 1;
+        System.out.println(newVariable);
+        
+        newVariable = 5;
+        
+        System.out.println(newVariable);
+        
+        String myVariableChain = "string sample";
+        
+        System.out.println(myVariableChain);
+        
+        myVariableChain = "testing";
+        
+        System.out.println(myVariableChain);
+        
+        var myvar = 5;
+        
+        System.out.println(myvar);
+        
+        var testingagain = "7";
+        
+        System.out.println("testingagain = " + testingagain);
+    }
+}
